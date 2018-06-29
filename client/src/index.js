b9536e1d703ed5c67db6d5caad3c4fda19c5c0a1
Everett Quebral <Everett.Quebral@gmail.com>
2018-06-28 17:05:31 -0700

    adding wslink for the client

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import App from './App'
 import Page from './components/Page'
 import registerServiceWorker from './registerServiceWorker'
 import { HttpLink, InMemoryCache, ApolloClient } from 'apollo-boost'
+import { WebSocketLink } from 'apollo-link-ws'
 import { ApolloLink, split } from 'apollo-link'
 import { ApolloProvider } from 'react-apollo'
 import { getMainDefinition } from 'apollo-utilities'
@@ -12,21 +13,37 @@ import Container from './components/Container'
 
 
 const httpLink = new HttpLink({ uri: 'http://127.0.0.1:4000/graphql'})
+const wsLink = new WebSocketLink({
+  uri: 'ws://127.0.0.1/4000',
+  options: {
+    reconnect: true
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink
+)
 
 const client = new ApolloClient({
-    link : ApolloLink.from([httpLink]),
-    cache: new InMemoryCache(),
-    connectToDevTools: true
+  // link : ApolloLink.from([httpLink]),
+  link: ApolloLink.from([link]),
+  cache: new InMemoryCache(),
+  connectToDevTools: true
 })
 
 
 //ReactDOM.render(<Page />, document.getElementById('root'));
 
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <Container />
-    </ApolloProvider>,
-    document.getElementById('root')
+  <ApolloProvider client={client}>
+    <Container />
+  </ApolloProvider>,
+  document.getElementById('root')
 )
 
 registerServiceWorker();
