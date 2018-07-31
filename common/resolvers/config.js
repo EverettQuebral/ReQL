659729b446d46a779fa10e96e331c649797b23a9
Everett Quebral <Everett.Quebral@gmail.com>
2018-07-30 19:49:01 -0700

    adding Reactive UI support

--- common/resolvers/config.js
+++ common/resolvers/config.js
@@ -1,19 +1,58 @@
 
-const config = {
-  type: "Page",
-  props: {
-    className: "page",
-    id: "page-1",
-    src: ""
-  },
-  attributes: {
-    text: "Welcome Page"
-  }
+const components = {
+  "type": "Container",
+  "children" : [
+    {
+      "type": "Jumbotron",
+      "children": [
+        {
+          "type": "Primitive",
+          "content": `<h1 className='display-3'>Reactive UI</h1>`
+        },
+        {
+          "type": "Primitive",
+          "content": `<hr />`
+        },
+        {
+          "type": "Primitive",
+          "content": `<p>An Example of Reactive UI where everything you see here are all part of a configuration that are dynamically created as React Components</p>`
+        }]
+    },
+    {
+      "type": "Container",
+      "children":
+      [{
+          "type": "Alert",
+          "props": {
+            "color": "primary"
+          },
+          "content": "Text in Blue Here"
+        },
+        {
+          "type": "Alert",
+          "props": {
+            "color": "secondary"
+          },
+          "content": "Text in Gray Here"
+        },
+        {
+          "type": "Alert",
+          "props": {
+            "color": "success"
+          },
+          "content": "Text in Green Here"
+        },
+        {
+          "type": "Button",
+          "content": "All you are seeing here are dynamically generated from a JSON Config file"
+        }]
+    }
+  ]
 }
 export default {
   Query: {
     getConfig: (root, args, context) => {
-      return config
+      return components
     }
   }
 }
